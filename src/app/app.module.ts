c1839c2008c3007607be15244a81b418523977f5
rejkid <rejkid@gmail.com>
2023-09-04 12:57:56 +1000

    Some fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+//import { MaterialModule } from './material/material.module';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatTableModule,
+    MatCheckboxModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
